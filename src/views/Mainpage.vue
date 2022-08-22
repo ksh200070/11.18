fcabe837617a44b7c3754b2529442e1151c6fffb
ksh200070 <rlatmdgus1120@gmail.com>
2022-08-22 14:49:32 +0900

    open 오류 수정, 모두 잘 작동되는것 확인

--- src/views/Mainpage.vue
+++ src/views/Mainpage.vue
@@ -477,7 +477,15 @@ export default {
       this.Q_gather_page=false;
       this.qna_answer_page=true;
 
-      this.dayNum = event.currentTarget.innerText[1];
+      if (isNaN(event.currentTarget.innerText[2])){
+        // console.log('true : 숫자가 아님')
+        this.dayNum = event.currentTarget.innerText[1];
+      }
+      else{
+        // console.log('false : 숫자임')
+        this.dayNum = event.currentTarget.innerText.substring(1,3);
+      }
+      // this.dayNum = event.currentTarget.innerText[1];
       // console.log('그냥 타겟 : '+event.target)
       // console.log('그냥 타겟(이너텍스트) : '+event.target.innerText)
       // console.log('현재 타겟 : '+event.currentTarget)
@@ -732,16 +740,17 @@ export default {
          this.dayNum = parseInt(event.target.nextSibling.data);
          console.log(this.dayNum)
        }
-       axios
-       .get('http://localhost:3001/api/members/question',this.config)
-       .then(res=> {
-         console.log('다시 받아온 opend값:'+res.data.result.question[this.dayNum-1].opened)
-       })
+      //  axios
+      //  .get('http://localhost:3001/api/members/question',this.config)
+      //  .then(res=> {
+      //    console.log('다시 받아온 opend값:'+res.data.result.question[this.dayNum-1].opened)
+      //  })
       this.opened = this.userInfo.question[this.dayNum-1].opened;
       this.answerY_N = this.userInfo.question[this.dayNum-1].answerY_N; 
-      this.getBoxInfo()
-      console.log("opened:" + this.opened, 'answerY_N:'+this.answerY_N)
+      // this.getBoxInfo()
+      console.log("처음 데이터베이스 상 opened:" + this.opened, 'answerY_N:'+this.answerY_N)
 
+      //답변 입력 페이지 -> qNum 전달 > 질문, 답변 받아오기
       let config2 = {
               headers : {
                 'access-token': this.token
@@ -754,10 +763,39 @@ export default {
             axios
             .get('http://localhost:3001/api/members/qnapage', config2)
             .then(res => {
-              console.log(res.data.result)
-              this.q= res.data.result.qnacontent
-              this.a= res.data.result.answer
-            })
+              if(res.data.message == '성공'){
+                console.log(res.data.message)
+                this.q= res.data.result.qnacontent
+                this.a= res.data.result.answer
+                axios
+                .get('http://localhost:3001/api/members/question',this.config)
+                .then(res=> {
+                  console.log('qNum 전달 후 다시 받아온 opend값:'+res.data.result.question[this.dayNum-1].opened)
+                  this.opened = res.data.result.question[this.dayNum-1].opened
+                  if(this.answerY_N==0){
+                    setTimeout(function(){
+                    this.qna_answer_page=true;
+                    this.loading_page=false;
+                    }.bind(this),2000)
+                    this.loading_page=true;
+                    this.Q_list_page=false;
+                  }
+                  else if(this.answerY_N==1){
+                    this.qna_answer_page=true;
+                    this.Q_list_page=false;
+                  }
+                })
+              }
+              else if(res.data.message == '아직 오픈 시간이 되지 않았습니다.'){
+                console.log(res.data.message)
+                this.ooops=true;
+              }
+              })
+              
+
+
+              
+            // })
             .catch(err => {
              console.log(err);
             })
@@ -766,34 +804,16 @@ export default {
 
       // };
     },
-    getBoxInfo(){
-      axios
-       .get('http://localhost:3001/api/members/question',this.config)
-       .then(res=> {
-         console.log('다시 받아온 opend값:'+res.data.result.question[this.dayNum-1].opened)
-         this.opened = res.data.result.question[this.dayNum-1].opened
-         console.log(this.opened)
-         if(this.opened==0){
-          this.ooops=true;
-        }
-        else if(this.opened==1){
-          // console.log('dayNum:'+ this.dayNum);
-
-          if(this.answerY_N==0){
-            setTimeout(function(){
-              this.qna_answer_page=true;
-              this.loading_page=false;
-            }.bind(this),2000)
-            this.loading_page=true;
-            this.Q_list_page=false;
-          }
-          else if(this.answerY_N==1){
-            this.qna_answer_page=true;
-            this.Q_list_page=false;
-          }
-        }
-       })
-    },
+    // getBoxInfo(){
+    //   axios
+    //    .get('http://localhost:3001/api/members/question',this.config)
+    //    .then(res=> {
+    //      console.log('다시 받아온 opend값:'+res.data.result.question[this.dayNum-1].opened)
+    //      this.opened = res.data.result.question[this.dayNum-1].opened
+    //      console.log(this.opened)
+         
+    //    })
+    // },
     togo_setting_page() {
       this.setting_page=true;
     },
